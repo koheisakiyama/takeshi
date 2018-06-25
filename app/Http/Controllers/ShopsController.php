2dc79f22c0b10ffccde649fa8ca9ffe0738f1e3a
k-koda <keisuke.physi@gmail.com>
2018-06-25 10:26:58 +0900

    コントーラーとビューファイルを作成

---
+++ app/Http/Controllers/ShopsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class ShopsController extends Controller
+{
+    public function index() {
+      return view('shops.index');
+    }
+    public function search() {
+      return view('shops.search');
+    }
+}
