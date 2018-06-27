5b3d47dc7a566206d7f6333a7726f12376b368eb
tech-camp <tech-camp@1-181.local>
2018-06-27 13:16:57 +0900

    カテゴリーのラジオボタン作成

---
+++ database/migrations/2018_06_27_024114_create_shops_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('address');
+            $table->string('area');
+            $table->float('lat');
+            $table->float('lon');
+            $table->string('category');
+            $table->integer('origami');
+            $table->integer('rakuten');
+            $table->integer('line');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('shops');
+    }
+}
