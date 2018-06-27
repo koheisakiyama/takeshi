8e939bf0840cd2aa48c3763f8e5ab853c6561a88
k-koda <keisuke.physi@gmail.com>
2018-06-27 11:39:32 +0900

    ShopsControllerの編集

--- app/Http/Controllers/ShopsController.php
+++ app/Http/Controllers/ShopsController.php
@@ -8,5 +8,7 @@ use App\Http\Requests;
 
 class ShopsController extends Controller
 {
-    //
+    public function index() {
+      return view ('shops.index');
+    }
 }
--- resources/views/shops/index.blade.php
+++ resources/views/shops/index.blade.php
@@ -19,17 +19,8 @@
     </style>
   </head>
     <body>
-    <div id="map"></div>
-    <script>
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
-    async defer></script>
+    <p>
+    Hello World!
+    </p>
   </body>
 </html>
