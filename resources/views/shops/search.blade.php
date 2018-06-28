8516424bf5cc092e01cf8c5bbb1dd263e72e392a
k-koda <keisuke.physi@gmail.com>
2018-06-28 12:07:34 +0900

    リストの表示

--- resources/views/shops/search.blade.php
+++ resources/views/shops/search.blade.php
@@ -4,6 +4,7 @@
     <title>Simple Map</title>
     <meta name="viewport" content="initial-scale=1.0">
     <meta charset="utf-8">
+    <link href="{{ asset('assets/css/bootstrap.min.css')}}" rel='stylesheet' type='text/css'>
     <style>
       /* Always set the map height explicitly to define the size of the div
        * element that contains the map. */
@@ -11,7 +12,7 @@
         height: 100%;
       }
       /* Optional: Makes the sample page fill the window. */
-      html, body {
+      html, body, div.row, div.container, div.mapArea{
         height: 100%;
         margin: 0;
         padding: 0;
@@ -20,31 +21,51 @@
   </head>
 
   <body>
-    <div id="map"></div>
 
-    <script>
-      function initMap() {
-        /* 地図の中心 20180628 kkoda*/
-        var ll = {lat: 35.6284, lng: 139.736571};
-        var map = new google.maps.Map(
-          document.getElementById('map'), { center: ll, zoom: 14}
-        );
 
-        /* マーカーをつけるllを準備 20180628 kkoda */
-        var arr = [];
-        var markerPosi00 = {lat: 35.6123, lng:139.7724};
-        var markerPosi01 = {lat: 35.6322, lng:139.7744};
-        var markerPosi02 = {lat: 35.6212, lng:139.7274};
-        arr[0] = markerPosi00;
-        arr[1] = markerPosi01;
-        arr[2] = markerPosi02;
-        /* マーカーを表示 20180628 kkoda */
-        for(let i = 0; i < arr.length; i++) {
-          var marker = new google.maps.Marker({position:arr[i] , map:map});
-        }
-      }
-    </script>
+    <div class="container">
+      <div class="row">
+        <div class="result-list col-xs-2 col-sm-2 col-md-2 col-lg-2">
+          <ul class="list-group">
+            <li class="list-group-item">
+              Hello World!
+            <a href="/#">
+            ルート
+            </a>
+            </li>
+            <li class="list-group-item">
+              Hoge Hoge
+            <a href="/#">
+            ルート
+            </a>
+            </li>
+        </div>
+        <div id="map" class="mapArea col-xs-10 col-sm-10 col-md-10 col-lg-10">
+        </div>
+      </div>
+    </div>
+        <script>
+          function initMap() {
+            /* 地図の中心 20180628 kkoda*/
+            var ll = {lat: 35.6284, lng: 139.736571};
+            var map = new google.maps.Map(
+              document.getElementById('map'), { center: ll, zoom: 14}
+            );
 
+            /* マーカーをつけるllを準備 20180628 kkoda */
+            var arr = [];
+            var markerPosi00 = {lat: 35.6123, lng:139.7424};
+            var markerPosi01 = {lat: 35.6322, lng:139.7344};
+            var markerPosi02 = {lat: 35.6212, lng:139.7274};
+            arr[0] = markerPosi00;
+            arr[1] = markerPosi01;
+            arr[2] = markerPosi02;
+            /* マーカーを表示 20180628 kkoda */
+            for(let i = 0; i < arr.length; i++) {
+              var marker = new google.maps.Marker({position:arr[i] , map:map});
+            }
+          }
+        </script>
     <script src="https://maps.googleapis.com/maps/api/js?key={{ env('Google_API_Key') }}&callback=initMap"
     async defer></script>
 
