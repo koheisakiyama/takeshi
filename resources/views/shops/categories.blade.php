55c6457474416345aaf09fc2e7ca9fb5343c06f4
k-koda <keisuke.physi@gmail.com>
2018-06-29 12:25:18 +0900

    Revert "カテゴリーのラジオボタン修正"
    This reverts commit 243562f5d99dfd2d67aa31539d2c0493eef718a1.

--- resources/views/shops/categories.blade.php
+++ resources/views/shops/categories.blade.php
@@ -16,12 +16,14 @@
   <body>
     <div class="btn-group">
   <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Categories <span class="caret"></span>
+    Action <span class="caret"></span>
   </button>
   <ul class="dropdown-menu">
-    <li><a href="#">Restaurant</a></li>
-    <li><a href="#">Beauty</a></li>
-    <li><a href="#">Variety store</a></li>
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li role="separator" class="divider"></li>
+    <li><a href="#">Separated link</a></li>
   </ul>
 </div>
   </body>
