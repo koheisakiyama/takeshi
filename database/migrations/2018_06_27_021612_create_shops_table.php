2d5228e9e43fcc43257443a89d5ed5107da65fc0
k-koda <keisuke.physi@gmail.com>
2018-06-29 12:38:37 +0900

    Revert "DBのカラム編集"
    This reverts commit 07f7e1e39a697e0c6d762c01222ee06b1f582083.

--- database/migrations/2018_06_27_021612_create_shops_table.php
+++ database/migrations/2018_06_27_021612_create_shops_table.php
@@ -14,15 +14,6 @@ class CreateShopsTable extends Migration
     {
         Schema::create('shops', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->text('address');
-            $table->string('area');
-            $table->float('lat');
-            $table->float('lon');
-            $table->string('category');
-            $table->integer('origami');
-            $table->integer('rakuten');
-            $table->integer('line');
             $table->timestamps();
         });
     }
