c5d28d43b636532e63dfca9420a018bfe0335860
tech-camp <r.suganuma6766@gmail.com>
2018-07-03 11:21:27 +0900

    Merge branch 'search_test' of github.com:koheisakiyama/takeshi into serch_test_ver.takeshi
    # Conflicts:
    #	resources/views/shops/suganuma.html

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -29,10 +29,10 @@
       }
       .open {
         z-index: 2;
-        width: 50px;
-        height: 50px;
+        width: 150px;
+        height: 30px;
         color: white;
-        background-color: lightSeaGreen;
+        background-color: #A3D1FF;
         font-size: 2em;
         line-height: 50px;
         text-align: center;
@@ -88,14 +88,14 @@
         <!-- <a href="" class="center-block">pay serch</a> -->
       <!-- </div><div align="center"> -->
       <!-- </h1> -->
-    <nav class="navbar navbar-default" style="background-color: #A3D1FF;">
+    <nav class="navbar navbar-inverse navbar-fixed-top" style="background-color: #A3D1FF;">
       <div class="container-fluid">
         <div class="navbar-header">
           <!-- <a class="navbar-brand" href="#">Bootstrap3 チュートリアル</a> -->
         </div>
        {{ Form::open(['action' => 'ShopsController@result', 'method' => 'get']) }}
         <ul class="nav navbar-nav" style="padding: 0px;">
-          <li class="active"><a href="#" style="padding: 0;background-color: #A3D1FF; margin:0;"><h1 style="margin: 0px;">pay search</h1></a></li>
+          <li class="active"><a href="#" style="padding: 0;background-color: #A3D1FF; margin:0;"><h1 style="margin: 10px;">pay search</h1></a></li>
           <li><a href="#">@include ('shops.details.how')</a></li>
           <li><a href="#">@include ('shops.details.what')</a></li>
           <li><a href="#">@include ('shops.details.where')</a></li>
