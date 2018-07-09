171d61f5d19f6313e04e011e0d30597666b34b30
tech-camp <r.suganuma6766@gmail.com>
2018-07-09 10:22:58 +0900

    編集ファイル：layout.blade.php 編集結果：検索ボタン統一 編集内容：102行目　削除

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -99,12 +99,11 @@
           <li><a href="#">@include ('shops.details.how')</a></li>
           <li><a href="#">@include ('shops.details.what')</a></li>
           <li><a href="#">@include ('shops.details.where')</a></li>
-          <li><a href="#">{{ Form::submit('検索', ['class' => 'btn btn-primary navbar-form']) }}</a></li>
           <li>      
             <div style="margin: 15px 0">
             {{ Form::textarea('review', '', ['placeholder' => 'キーワードを入力してください', 'style' => 'width: 55s%;height: 30px;']) }}
             </div></li><!-- フリーワード検索ボックス -->
-          <li><a href="#">{{ Form::submit('検索', ['class' => 'btn btn-primary navbar-form']) }}</a></li><!-- フリーワード検索ボタン -->
+          <li><a href="#">{{ Form::submit('検索', ['class' => 'btn btn-primary navbar-form']) }}</a></li><!-- 検索ボタン -->
         </ul>
         {{ Form::close() }}
       </div>
