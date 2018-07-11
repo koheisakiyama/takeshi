68d97fefd5d9337d63c9c1cf061e361b52f2b4c9
k-koda <keisuke.physi@gmail.com>
2018-07-11 13:10:52 +0900

    現在地のマーカーを設置

--- resources/views/shops/navi.blade.php
+++ resources/views/shops/navi.blade.php
@@ -14,41 +14,36 @@
   var map = null;     // 地図オブジェクト
   var marker = null;  // マーカーオブジェクト
   var watchId = null; // 監視対象の地図のID
-  if (navigator.geolocation) {
-    var id = navigator.geolocation.watchPosition(
-              initMap, 
-              onError, 
-              {
-                enableHighAccuracy : true,
-                maximumAge         : 60000,
-                timeout            : 55000,
-                frequency          : 30000
-    });
-  } else {
-    window.alert('Geolocation API対応ブラウザでアクセスしてください。');
+  var current = null; // 現在地
 
-  }
-  // エラー時のコールバック関数
-  function onError(error) {
-      alert('コード: '        + error.code    + '\n' +
-              'メッセージ: '    + error.message + '\n');
-  }
-
+  // 現在地取得のオプション
+  var getOpt = {
+    enableHighAccuracy : true,
+    maximumAge         : 10000,
+    timeout            : 9000,
+  };
+  
+  navigator.geolocation.getCurrentPosition(
+            initMap,
+            onError, 
+            getOpt
+  );
+  
   function initMap(position) {
   //出発地をidから引き出したlatlonに代入する
-    console.log(position.coords.latitude);
-
-    var s_latlng = new google.maps.LatLng(<?php echo json_encode($s_latlng); ?>); 
+    current = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    //var s_latlng = new google.maps.LatLng(<?php echo json_encode($s_latlng); ?>); 
+    var s_latlng = current;
     var g_latlng = new google.maps.LatLng(<?php echo json_encode($g_latlng); ?>); 
-    var center = new google.maps.LatLng(<?php echo json_encode($s_latlng); ?>); 
-    var current = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    //var center = new google.maps.LatLng(<?php echo json_encode($s_latlng); ?>); 
     var directionsService = new google.maps.DirectionsService();
     var directionsRenderer = new google.maps.DirectionsRenderer();
 
     // 地図初期化のオプション
     var mapOptions = {
         zoom: 17,
-        center: center,
+        //center: center,
+        center: current,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         scaleControl: true,
     };
@@ -66,13 +61,38 @@
       directionsRenderer.setDirections(result); // 取得したルートをセット
       directionsRenderer.setMap(map); // ルートを地図に表示
       directionsRenderer.setPanel(document.getElementById('directions_panel')); // 道順を表示する k-koda
+      //console.log(result.routes[0].overview_path[1].lat());
     });
 
-    marker = new google.maps.Marker({
+    // マーカーの作成と表示
+    marker = new google.maps.Marker({ 
       position: current,
       map: map
     });
   }
+
+  // 移動した時の現在地をマーカーで表示
+  function setMarker(position){ 
+    marker.setMap(null); // すで表示されているマーカーを削除
+    current = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    // マーカーの作成と表示
+    marker = new google.maps.Marker({ 
+      position: current,
+      map: map
+    });
+  }
+  // 移動時の現在地の取得
+  navigator.geolocation.watchPosition(
+            setMarker,
+            onError, 
+            getOpt
+  );
+  // エラー時のコールバック関数
+  function onError(error) {
+      alert('コード: '        + error.code    + '\n' +
+              'メッセージ: '    + error.message + '\n');
+  }
+
 
 </script>
 @endsection
