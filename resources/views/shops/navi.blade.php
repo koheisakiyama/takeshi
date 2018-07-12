b5fb5edabe34f93ba85b0ee764c6dd9b1129d9bf
k-koda <keisuke.physi@gmail.com>
2018-07-12 13:16:55 +0900

    ステップの始まりに近づくとポップアップするための条件

--- resources/views/shops/navi.blade.php
+++ resources/views/shops/navi.blade.php
@@ -11,10 +11,15 @@
 
 <script type="text/javascript">
 
-  var map = null;     // 地図オブジェクト
-  var marker = null;  // マーカーオブジェクト
-  var watchId = null; // 監視対象の地図のID
-  var current = null; // 現在地
+  var map = null;        // 地図オブジェクト
+  var marker = null;     // マーカーオブジェクト
+  var watchId = null;    // 監視対象の地図のID
+  var current = null;    // 現在地
+  var errPos = null;     // 位置測定の誤差 単位はメートル
+  var route = null;
+  var stepLatLng = null; // ステップごとの緯度経度
+  var stepNum = 0;
+  var stepComment = null;
 
   // 現在地取得のオプション
   var getOpt = {
@@ -22,16 +27,24 @@
     maximumAge         : 10000,
     timeout            : 9000,
   };
+  // 誤差円のオプション
+  var cirOpt = {
+    map: map,
+    center: current,
+    radius: errPos, // 単位はメートル
+    strokeColor: '#0088ff',
+    strokeOpacity: 0.8,
+    strokeWeight: 1,
+    fillColor: '#0088ff',
+    fillOpacity: 0.2
+  };
   
-  navigator.geolocation.getCurrentPosition(
-            initMap,
-            onError, 
-            getOpt
-  );
+  navigator.geolocation.getCurrentPosition(initMap, onError,  getOpt);
   
   function initMap(position) {
   //出発地をidから引き出したlatlonに代入する
-    current = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    current = new google.maps.LatLng(position.coords.latitude, position.coords.longitude); // 現在地の緯度経度取得
+    errPos = position.coords.accuracy; // 位置の誤差を取得
     //var s_latlng = new google.maps.LatLng(<?php echo json_encode($s_latlng); ?>); 
     var s_latlng = current;
     var g_latlng = new google.maps.LatLng(<?php echo json_encode($g_latlng); ?>); 
@@ -45,14 +58,14 @@
         //center: center,
         center: current,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        scaleControl: true,
+        //scaleControl: true,
     };
     // 地図を表示
     map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
     // ルートを取得
     var request = {
-      origin: s_latlng,        // 出発地点の緯度、経度
+      origin:      s_latlng,   // 出発地点の緯度、経度
       destination: g_latlng,   // 到着地点の緯度、経度
       //travelMode: google.maps.DirectionsTravelMode.DRIVING // ルートの種類
       travelMode: google.maps.DirectionsTravelMode.WALKING // ルートの種類
@@ -61,32 +74,42 @@
       directionsRenderer.setDirections(result); // 取得したルートをセット
       directionsRenderer.setMap(map); // ルートを地図に表示
       directionsRenderer.setPanel(document.getElementById('directions_panel')); // 道順を表示する k-koda
-      //console.log(result.routes[0].overview_path[1].lat());
+      route = result.routes[0].legs[0].steps;
+      stepLatLng = route[0].start_location; // ステップの緯度を取得
+      stepComment = route[0].instructions; // ステップの説明を取得
     });
 
     // マーカーの作成と表示
-    marker = new google.maps.Marker({ 
-      position: current,
-      map: map
-    });
+    marker = new google.maps.Marker({position: current, map: map});
+    // 誤差を円で描く
+    //new google.maps.Circle(cirOpt);
   }
 
   // 移動した時の現在地をマーカーで表示
   function setMarker(position){ 
     marker.setMap(null); // すで表示されているマーカーを削除
-    current = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    current = new google.maps.LatLng(position.coords.latitude, position.coords.longitude); // 現在地の緯度経度取得
+    errPos = position.coords.accuracy; // 位置の誤差を取得
     // マーカーの作成と表示
-    marker = new google.maps.Marker({ 
-      position: current,
-      map: map
-    });
+    marker = new google.maps.Marker({position: current, map: map});
+    // 誤差を円で描く
+    //new google.maps.Circle(cirOpt);
+    //
+    var diffLat = current.lat() - stepLatLng.lat();
+    var diffLng = current.lng() - stepLatLng.lng();
+    console.log(Math.pow(diffLat,2));
+    console.log(Mngh.pow(diffLng,2));
+    // 2018/07/12 ここまで直線距離を計算するところまで
+    //if ( < 0.0001) {
+    //  function (){
+    //  }
+    //  stepNum++;
+    //  stepLatLng = route[stepNum].start_location; // ステップの緯度を取得
+    //  stepComment = route[stepNum].instructions; // ステップの説明を取得
+    //}
   }
   // 移動時の現在地の取得
-  navigator.geolocation.watchPosition(
-            setMarker,
-            onError, 
-            getOpt
-  );
+  navigator.geolocation.watchPosition(setMarker, onError, getOpt);
   // エラー時のコールバック関数
   function onError(error) {
       alert('コード: '        + error.code    + '\n' +
