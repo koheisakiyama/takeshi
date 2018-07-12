549332d8bcf2e418873ca4e9b0682705ae5934f5
koheisakiyama <maccharatake00@outlook.jp>
2018-07-12 12:16:02 +0900

    余分なスペース削除
    余分なスペース削除

--- app/Http/Controllers/ShopsController.php
+++ app/Http/Controllers/ShopsController.php
@@ -26,7 +26,7 @@ class ShopsController extends Controller
       //先にエリアとカテゴリーで検索をかける。
       $withoutService = Shop::where('area', $area)->where('category', $category)->get();
       //ビューに渡すようのコレクションを用意する
-      $shops = collect(); 
+      $shops = collect();
       //それぞれのサービスごとに検索
       foreach((array) $request->method as $method){
         //それぞれのコレクションをshopsに追加する。
@@ -48,10 +48,10 @@ class ShopsController extends Controller
           $latlng = ['lat'=>35.65803, 'lng'=>139.699447];
           break;
       }
-      
+
       return view ('shops.result') -> with(['shops' => $shops, 'latlng'=>$latlng]);
     }
-    
+
     public function show($id){
     // ルート表示のコントローラー
     //出発地(検索で選択された場所)と目的地(クリックされた店のid)のlatとlonを取得、ビューに渡す。seina
@@ -68,7 +68,7 @@ class ShopsController extends Controller
           $lon1 = 139.69821;
           break;
         case '品川':
-          $lat1 = 35.6284; 
+          $lat1 = 35.6284;
           $lon1 = 139.736571;
           break;
         case '渋谷':
@@ -76,9 +76,9 @@ class ShopsController extends Controller
           $lon1 = 139.699447;
           break;
       }
-        
+
       $lat2 = $shop_id->lat; //取得したレコードのlat,lonを取得。したい。。
-      $lon2 = $shop_id->lon; 
+      $lon2 = $shop_id->lon;
 
       return view('shops.road')->with(array('lat1'=>$lat1,'lon1'=>$lon1, 'lat2'=>$lat2 ,'lon2'=> $lon2));
     }
