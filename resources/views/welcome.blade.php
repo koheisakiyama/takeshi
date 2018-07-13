7fb89d82dcda68c1ebe0e22172c626f6d7e9224a
koheisakiyama <maccharatake00@outlook.jp>
2018-07-13 11:10:30 +0900

    ウェルカム画面再編集
    タイトル変更
    背景変更　etc...

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,19 +15,25 @@
                 height: 100%;
             }
 
+            /*コードの順番変更*/
+             .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
             body {
                 margin: 0;
                 padding: 0;
                 width: 100%;
                 display: table;
                 font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
                 text-align: center;
-                display: table-cell;
-                vertical-align: middle;
+                font-family: 'Lato';
+                /*背景変更*/
+                background: #1c92d2;  /* fallback for old browsers */
+                background: -webkit-linear-gradient(to right, #f2fcfe, #1c92d2);  /* Chrome 10-25, Safari 5.1-6 */
+                background: linear-gradient(to right, #f2fcfe, #1c92d2); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
             }
 
             .content {
@@ -39,14 +45,27 @@
                 font-size: 96px;
             }
 
+            /*テキスト追加*/
+            .text {
+                font-size: 30px;
+                color: #74748b;
+            }
+
+            /*ボタン位置変更*/
+            .white {
+                margin: 20px;
+            }
+
+
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
                 <div class="title">pay search</div>
+                <div class="text">あなたのキャッシュレス決済を、もっと賢く、スマートに</div>
                 <!--ウェルカム画面のボタンを押すとindexに遷移する -->
-                <a href="/index" class="btn btn-primary btn-lg">
+                <a href="/index" class="btn btn-primary btn-lg white">
                 <i class="glyphicon glyphicon-ruble"></i>Lets start!!!</a>
         </div>
     </body>
