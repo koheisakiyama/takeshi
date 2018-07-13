e0a8d7e5592f0bd6da9278f0ad377792aad335e5
k-koda <keisuke.physi@gmail.com>
2018-07-13 11:34:20 +0900

    ポップアップでナビ

--- resources/views/shops/navi.blade.php
+++ resources/views/shops/navi.blade.php
@@ -8,6 +8,22 @@
 <!-- 
   <script src="{{ asset('assets/javascripts/shopRoad.js') }}"></script>
    -->
+  <div class="modal fade" id="sampleModal" tabindex="-1">
+  	<div class="modal-dialog">
+  		<div class="modal-content">
+  			<div class="modal-body">
+          <ul style="list-style:none;">
+            <li><p><span id="step_comment"></span></p></li>
+            <li><p>距離：<span id="step_distance"></span></p></li>
+            <li><p>時間：<span id="step_duration"></span></p></li>
+          </ul>
+  			</div>
+  			<div class="modal-footer">
+  				<button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
+  			</div>
+  		</div>
+  	</div>
+  </div>
 
 <script type="text/javascript">
 
@@ -16,10 +32,14 @@
   var watchId = null;    // 監視対象の地図のID
   var current = null;    // 現在地
   var errPos = null;     // 位置測定の誤差 単位はメートル
+  var errCir = null;     // 誤差の範囲
   var route = null;
-  var stepLatLng = null; // ステップごとの緯度経度
-  var stepNum = 0;
-  var stepComment = null;
+  var stepLat = null; // ステップごとの緯度
+  var stepLng = null; // ステップごとの経度
+  var stepNum = 0; // ステップ番号
+  var stepDistance = null; // ステップごとの距離
+  var stepDuration = null; // ステップごとの時間
+  var stepComment = null; // ステップごとの説明
 
   // 現在地取得のオプション
   var getOpt = {
@@ -75,38 +95,72 @@
       directionsRenderer.setMap(map); // ルートを地図に表示
       directionsRenderer.setPanel(document.getElementById('directions_panel')); // 道順を表示する k-koda
       route = result.routes[0].legs[0].steps;
-      stepLatLng = route[0].start_location; // ステップの緯度を取得
+      //for(var i in route){
+        //console.log(route[i].instructions);
+      //}
+      stepLat = route[0].start_location.lat(); // ステップの緯度を取得
+      stepLng = route[0].start_location.lng(); // ステップの緯度を取得
       stepComment = route[0].instructions; // ステップの説明を取得
+      stepDuration = route[0].duration.text; // ステップの時間を取得
+      stepDistance = route[0].distance.text; // ステップの距離を取得
+      //console.log(stepDuration);
+      //console.log(stepDistance);
+      $(function (){
+        document.getElementById('step_comment').innerHTML=stepComment;
+        document.getElementById('step_duration').innerHTML=stepDuration;
+        document.getElementById('step_distance').innerHTML=stepDistance;
+        console.log(stepComment);
+        $('#sampleModal').modal();
+      });
+      stepLat = route[stepNum].start_location.lat(); // ステップの緯度を取得
+      stepLng = route[stepNum].start_location.lng(); // ステップの緯度を取得
+      stepComment = route[stepNum].instructions; // ステップの説明を取得
+      stepDuration = route[stepNum].duration.text; // ステップの時間を取得
+      stepDistance = route[stepNum].distance.text; // ステップの距離を取得
+      stepNum++;
     });
 
     // マーカーの作成と表示
     marker = new google.maps.Marker({position: current, map: map});
     // 誤差を円で描く
-    //new google.maps.Circle(cirOpt);
+    errCir = new google.maps.Circle(cirOpt);
+    errCir.setMap(map);
   }
 
   // 移動した時の現在地をマーカーで表示
   function setMarker(position){ 
     marker.setMap(null); // すで表示されているマーカーを削除
+    errCir.setMap(null); // すで表示されている円を削除
     current = new google.maps.LatLng(position.coords.latitude, position.coords.longitude); // 現在地の緯度経度取得
     errPos = position.coords.accuracy; // 位置の誤差を取得
     // マーカーの作成と表示
     marker = new google.maps.Marker({position: current, map: map});
     // 誤差を円で描く
-    //new google.maps.Circle(cirOpt);
-    //
-    var diffLat = current.lat() - stepLatLng.lat();
-    var diffLng = current.lng() - stepLatLng.lng();
-    console.log(Math.pow(diffLat,2));
-    console.log(Mngh.pow(diffLng,2));
-    // 2018/07/12 ここまで直線距離を計算するところまで
-    //if ( < 0.0001) {
-    //  function (){
-    //  }
-    //  stepNum++;
-    //  stepLatLng = route[stepNum].start_location; // ステップの緯度を取得
-    //  stepComment = route[stepNum].instructions; // ステップの説明を取得
-    //}
+    errCir = new google.maps.Circle(cirOpt);
+    errCir.setMap(map);
+
+
+    // 2018/07/12 ここまで直線距離を計算する
+    var diffLat = Math.pow(current.lat() - stepLat,2);
+    var diffLng = Math.pow(current.lng() - stepLng,2);
+    // 近かったらmodalを表示
+    if (Math.sqrt(diffLat+diffLng) < 0.0001) {
+      $(function (){
+        //$('#sampleButton').click( function () {
+          document.getElementById('step_comment').innerHTML=stepComment;
+          document.getElementById('step_duration').innerHTML=stepDuration;
+          document.getElementById('step_distance').innerHTML=stepDistance;
+          console.log(stepComment);
+          $('#sampleModal').modal();
+        //});
+      });
+      stepLat = route[stepNum].start_location.lat(); // ステップの緯度を取得
+      stepLng = route[stepNum].start_location.lng(); // ステップの緯度を取得
+      stepComment = route[stepNum].instructions; // ステップの説明を取得
+      stepDuration = route[stepNum].duration.text; // ステップの時間を取得
+      stepDistance = route[stepNum].distance.text; // ステップの距離を取得
+      stepNum++;
+    }
   }
   // 移動時の現在地の取得
   navigator.geolocation.watchPosition(setMarker, onError, getOpt);
