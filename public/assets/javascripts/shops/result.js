fe0d4ad7df3c4b35343d15a95672de3c29a65576
lazward04 <lazward.alm@gmail.com>
2018-07-25 12:55:41 +0900

    現在地と店までの距離を計算して表示した

--- public/assets/javascripts/shops/result.js
+++ public/assets/javascripts/shops/result.js
@@ -1,9 +1,22 @@
+// 各
 function displayShops(latlng) {
   initMap(latlng);
-  for(var i in shops) {
-    var shopsLatLng = {lat:shops[i].lat, lng:shops[i].lon};
-    var shopsMarker = new google.maps.Marker({ position:shopsLatLng, map:map});
-  }
-  navigator.geolocation.getCurrentPosition(drawUserMarker, errorCallback);
+  console.log("piyo");
+  
+  navigator.geolocation.getCurrentPosition(function(position){
+      drawUserMarker(position);
+      current = new google.maps.LatLng(position.coords.latitude, position.coords.longitude); // 現在地の緯度経度取得
+      console.log(current);
+      for(var i in shops) {
+        var shopsLatLng = new google.maps.LatLng(shops[i].lat, shops[i].lon);
+        console.log(shopsLatLng);
+        var shopsMarker = new google.maps.Marker({ position:shopsLatLng, map:map});
+        let distance = Math.floor(google.maps.geometry.spherical.computeDistanceBetween(current,shopsLatLng));
+        document.getElementById('shop_' + shops[i].id).innerHTML= distance;
+      }
+    },
+    errorCallback,
+    getOpt
+  );
 }
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,8 +6,16 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
+        <script src="{{ asset('assets/javascripts/shops/errorCallback.js') }}"></script>
+        <script src="{{ asset('assets/javascripts/shops/initMap.js') }}"></script>
+        <script src="{{ asset('assets/javascripts/shops/drawUserMarker.js') }}"></script>
+        <script src="{{ asset('assets/javascripts/shops/currentLocation.js') }}"></script>
+        <script src="{{ asset('assets/javascripts/shops/result.js') }}"></script>
+        <script src="{{ asset('assets/javascripts/shops/navi.js') }}"></script>
+
     <link href="{{ asset('assets/css/bootstrap.min.css')}}" rel='stylesheet' type='text/css'>
     <link href="{{ asset('assets/css/bootstrap-multiselect.css')}}" rel='stylesheet' type='text/css'>
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ env('Google_API_Key') }}&libraries=geometry"></script>
     <script src="{{ asset('assets/javascripts/jquery-3.3.1.js') }}"></script>
     <script src="{{ asset('assets/javascripts/bootstrap.min.js') }}"></script>
     <script src="{{ asset('assets/javascripts/bootstrap-multiselect.js') }}"></script>
@@ -140,13 +148,7 @@
             timeout            : 9000,
           };
         </script>
-        <script src="{{ asset('assets/javascripts/shops/errorCallback.js') }}"></script>
-        <script src="{{ asset('assets/javascripts/shops/initMap.js') }}"></script>
-        <script src="{{ asset('assets/javascripts/shops/drawUserMarker.js') }}"></script>
-        <script src="{{ asset('assets/javascripts/shops/currentLocation.js') }}"></script>
-        <script src="{{ asset('assets/javascripts/shops/result.js') }}"></script>
-        <script src="{{ asset('assets/javascripts/shops/navi.js') }}"></script>
-        <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ env('Google_API_Key') }}"></script>
+        
         @yield ('content')
 
       </div>
